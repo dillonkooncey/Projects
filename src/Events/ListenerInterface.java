b5fd14652b1d64f05bab5d52901d96631842d4f0
Dillon Koonce <dckoonce@uncg.edu>
2019-09-29 02:09:58 -0400

    Added the author information and explained what class does.

--- src/Events/ListenerInterface.java
+++ src/Events/ListenerInterface.java
@@ -1,13 +1,13 @@
 package Events;
 
 /**
- *
- * @author Owner
+ * Interface that creates a messageRecieved() for other classes to user based on their needs.
+ * @author Dillon, Amina, Kumar. Last updated: September 29, 2019.
  */
 public interface ListenerInterface {
     /**
-     * Method that will be overridden by classes that listen for instructions.
-     * @param _event 
+     * Method that will be overridden by classes that listen for instructions and act upon them.
+     * @param _event - Event passed in from other classes.
      */
     public void messageRecieved(AppEvent _event);
 }
