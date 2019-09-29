327ca4ac13c6f5d43fa42bc0e95980979e03c3ff
Dillon Koonce <dckoonce@uncg.edu>
2019-09-29 02:10:36 -0400

    Added the author information and explained what class does.

--- src/Events/AppEvent.java
+++ src/Events/AppEvent.java
@@ -3,17 +3,18 @@ package Events;
 import java.util.EventObject;
 
 /**
- *
- * @author Owner
+ * Class that recognizes events taken in from other classes and creates new events
+ * and acts based on what the event is supposed to do.
+ * @author Dillon, Amina, Kumar. Last updated: September 29, 2019.
  */
 public class AppEvent extends EventObject{
     // Take in a message from the AppMessage class that will later be used in a method to display this message.
     private final AppMessage message;
     
     /**
-     * 
+     * Constructor that takes in new AppEvents from other classes.
      * @param _source - Takes in the name of the Object class the message came from.
-     * @param _message - Takes in the message from AppMessage class.
+     * @param _message - Takes in the desired message from the event passed in.
      */
     public AppEvent(Object _source, AppMessage _message) {
         super(_source);
@@ -21,8 +22,8 @@ public class AppEvent extends EventObject{
     }
     
     /**
-     * Displays the message from the original object class in which the message came from.
-     * @return  - Returns the message given from the original message from the object class.
+     * Gets the desired message from the event.
+     * @return  - Returns the message given from the event.
      */
     public AppMessage getMessage() {
         return this.message;
