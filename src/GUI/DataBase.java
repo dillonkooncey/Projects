416e69dd6ddad54917dd34e8dc932b87e7c9867d
Dillon Koonce <dckoonce@uncg.edu>
2019-10-06 16:58:25 -0400

    Fixed the removeUser() in DataBase class.

--- src/GUI/DataBase.java
+++ src/GUI/DataBase.java
@@ -36,14 +36,20 @@ public class DataBase {
      */
     public Boolean isUser(String _email, String _username, String _password) {
         try {
+            // establishing the connection.
             con = DriverManager.getConnection(host, userName, passWord);
+            // SQL statement to find a user that matches the email, username, and password entered.
             String SQL = "SELECT * FROM Users where Email = '" + _email + "' and Username = '" + _username + "' and Password = '" + _password + "'";
+            // Prepared statement with the SQL string.
             pst = con.prepareStatement(SQL);
+            // Create a result set with the SQL search.
             rs = pst.executeQuery();
+            // If statement for if there exists a user print the information matched and return true (true this user exists).
             if (rs.next()) {
                 System.out.println("Email, Username, and password matched");
                 con.close();
                 return true;
+            // Else the user does not exist in the database. Print that something didnt match and return false (False this user does not exist.).
             } else {
                 System.out.println("Something did not match");
                 con.close();
@@ -65,27 +71,35 @@ public class DataBase {
      */
     public Boolean addUser(String _email, String _username, String _password) {
         try {
+            // Establish a connection.
             con = DriverManager.getConnection(host, userName, passWord);
+            // Prepare a SQL statement looking for emails and usernames that match values entered.
             String SQL = "SELECT * FROM Users where Email = '" + _email + "' or Username = '" + _username + "'";
+            // Prepared statement included with the SQL search.
             pst = con.prepareStatement(SQL);
+            // Result set with results that match SQL search.
             rs = pst.executeQuery();
-
+            // If a user has a email or username matched with values entered the user cannot use this account.
             if (rs.next()) {
                 System.out.println("Username or email is taken choose another");
+                con.close();
+                // Retrun false we cannot add this user.
                 return false;
+                // Else the email and username eneterd are original and add the values to the database.
             } else {
                 String SQL2 = "INSERT INTO Users (Email, Username, Password) VALUES ('" + _email + "','" + _username + "','" + _password + "')";
                 stmt = con.createStatement();
                 stmt.executeUpdate(SQL2);
                 System.out.println("Email: " + _email + " Username: " + _username + " Password: " + _password + " added to DataBase");
+                con.close();
             }
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
+        // Return true we did add the values to the database.
         return true;
     }
     /**
-     * Method not completed.
      * Method that removes a user from the database.
      * @param _email - Email to be removed.
      * @param _username - Username to be removed.
@@ -93,10 +107,17 @@ public class DataBase {
      */
     public void removeUser(String _email, String _username, String _password) {
         try {
+            // Establish the connction.
             con = DriverManager.getConnection(host, userName, passWord);
-            String SQL = "DELETE FROM Users (Email, Username, Password) where Email = '" + _email + "', '" + _username + "', '" + _password + "'";
+            // SQl for deleting the account for the user.
+            String SQL = "DELETE FROM Users where Email = '" + _email + "' and Username = '" + _username + "' and Password = '" + _password + "'";
+            // Create a statement.
             stmt = con.createStatement();
+            // Execute the update desired with the SQL string.
             stmt.executeUpdate(SQL);
+            // Close the connection.
+            con.close();
+            // Print to the console that the user has been removed.
             System.out.println("Removed User");
         } catch(SQLException e) {
             System.out.println(e.getMessage());
