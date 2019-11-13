fed88baf10dfac9fe3132850d3607104535692c4
dillonkooncey <46939222+dillonkooncey@users.noreply.github.com>
2019-11-13 17:15:51 -0500

    Update ApiTranslator.java

--- Api/ApiTranslator.java
+++ Api/ApiTranslator.java
@@ -3,7 +3,7 @@ package Api;
 import java.util.ArrayList;
 /**
  * Translator class for the External API's.
- * @author Dillon. Last updated: November 4, 2019.
+ * @author Kumar, Amina. Last updated: November 4, 2019.
  */
 public class ApiTranslator {
     private static final ApiInterface connect = new Tmdb();
