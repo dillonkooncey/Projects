fd9280e84aa4df64b4a24838c4aa510a75f46cd6
dillonkooncey <46939222+dillonkooncey@users.noreply.github.com>
2019-11-18 14:50:22 -0500

    Update Tmdb.java

--- Api/Tmdb.java
+++ Api/Tmdb.java
@@ -1,13 +1,17 @@
 package Api;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 
 /**
  * Tmdb class that searches the Tmdb API for movies and actors.
  *
- * @author Kumar, Anima. Last updated: November 4, 2019.
+ * @author Kumar, Dillon. Last updated: November 18, 2019.
  */
 public class Tmdb implements ApiInterface {
+
     // Data fields for Tmdb class.
     private static final String apiKey = "68c9635b6b0bc9ccad7ea0b91402025b";
     private static final String baseUrl = "https://api.themoviedb.org";
@@ -21,10 +25,15 @@ public class Tmdb implements ApiInterface {
      */
     @Override
     public double searchMovie(String _movie) {
-        // Send the string to the string builder method.
-        String newString = this.searchStringBuilder(_movie);
-        // Build the string that will be sent to the API.
-        String searchString = Tmdb.baseUrl + "/3/search/movie?api_key=" + Tmdb.apiKey + "&language=en-US&query=" + newString + "&page=1&include_adult=false";
+        try {
+            // Send the string to the string builder method.
+            String newString = this.queryBuilder(_movie);
+            // Build the string that will be sent to the API.
+            String searchString = Tmdb.baseUrl + "/3/search/movie?api_key=" + Tmdb.apiKey + "&language=en-US&query=" + newString + "&page=1&include_adult=false";
+            return 0;
+        } catch (Exception e) {
+            System.out.println("there was an error: " + e.getMessage());
+        }
         return 0;
     }
 
@@ -37,10 +46,15 @@ public class Tmdb implements ApiInterface {
      */
     @Override
     public ArrayList<String> searchActor(String _actor) {
-        // Send the String to the string builder method.
-        String newString = this.searchStringBuilder(_actor);
-        // Build the string that will be sent to the API.
-        String searchString = Tmdb.baseUrl + "/3/search/person?api_key=" + Tmdb.apiKey + "&language=en-US&query=" + newString + "&page=1&include_adult=false";
+        try {
+            // Send Actor name to the searchStringBuilder().
+            String newString = this.queryBuilder(_actor);
+            // Build the search string to be used by the API.
+            String searchString = Tmdb.baseUrl + "/3/search/person?api_key=" + Tmdb.apiKey + "&language=en-US&query=" + newString + "&page=1&include_adult=false";
+            return null;
+        } catch (Exception e) {
+            System.out.println("There was an error: " + e.getMessage());
+        }
         return null;
     }
 
@@ -50,20 +64,13 @@ public class Tmdb implements ApiInterface {
      * @param _string - String searched by the user
      * @return - Newly constructed string.
      */
-    private String searchStringBuilder(String _string) {
-        // Split the string into a string array.
-        String[] splitString = _string.split(" ");
-        // Create a string that will be modified later.
-        String string = "";
-        // For loop to build a new string for the API search.
-        for (String i : splitString) {
-            string += i + "%20";
+    private String queryBuilder(String _string) {
+        try {
+            // Return the String modified by the URLEncoder class.
+            return URLEncoder.encode(_string, StandardCharsets.UTF_8.toString());
+        } catch (UnsupportedEncodingException ex) {
+            throw new RuntimeException(ex.getCause());
         }
-        // trim off the last "%20" from the for loop.
-        string = string.substring(0, string.length() - 3);
-        // return the newly built string.
-        return string;
-
     }
 
 }
