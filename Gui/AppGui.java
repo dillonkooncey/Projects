27ecad37a8f8f5f0cc35404b971b1366986ca73d
dillonkooncey <46939222+dillonkooncey@users.noreply.github.com>
2019-11-20 14:04:37 -0500

    Update AppGui.java

--- Gui/AppGui.java
+++ Gui/AppGui.java
@@ -13,8 +13,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
