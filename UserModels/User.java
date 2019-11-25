db87eef4e00835d4bc1363669188ae403aaebf7c
dillonkooncey <46939222+dillonkooncey@users.noreply.github.com>
2019-11-25 16:36:14 -0500

    Update User.java

--- UserModels/User.java
+++ UserModels/User.java
@@ -1,7 +1,6 @@
 package UserModels;
 
 import DataBase.DataBaseTranslator;
-import Events.AppMessage;
 import java.util.HashMap;
 
 /**
@@ -17,9 +16,8 @@ public class User {
     private String username;
     private String password;
     private String active = "true";
-    // Give User access to DataBaseTranslator class.
-    private final DataBaseTranslator translate = new DataBaseTranslator();
-
+    private static DataBaseTranslator translate = new DataBaseTranslator();
+    
     // Constuctor to construct new User object.
     public User(String _email, String _username, String _password) {
         this.email = _email;
@@ -36,25 +34,24 @@ public class User {
      * @param _email - Email passed in from login
      * @param _username - Username passed in from login
      * @param _password - Password passed in from login.
-     * @return - return AppMessage int based on whether or not object was found
-     * and saved.
+     * @return - return true if user was found or false if not.
      */
-    public int save(String _email, String _username, String _password) {
+    public static boolean validateUser(String _email, String _username, String _password) {
         // Create a HashMap and populate it with the information passed in from GUI and the string indicating what it is.
         HashMap<String, String> map = new HashMap();
         map.put("email", _email);
         map.put("username", _username);
         map.put("password", _password);
-        map.put("active", this.active);
-        // Int that equals the result of the DataBaseTranslator call.
-        int checkDb = this.translate.readObject(map, "users");
+        map.put("active", "true");
+        // boolean that equals the result of the DataBaseTranslator call.
+        boolean checkDb = translate.readObject(map, "users");
         // Check and see if user does not exist then create a new one. Else dont.
-        if (checkDb == 1) {
+        if (checkDb == true) {
             User user = new User(_email, _username, _password);
-            return AppMessage.HOME_SCREEN_PANEL;
+            return true;
             // Else the object didnt exist so return the int of the log in panel.
         } else {
-            return AppMessage.LOG_IN_PANEL;
+            return false;
         }
     }
 
@@ -67,23 +64,24 @@ public class User {
      * @param _email - Email entered in at registration menu.
      * @param _username - Username entered in at registration menu.
      * @param _password - Password entered in from registration menu.
-     * @return - AppMessage int corresponding to panel routed to.
+     * @return - True of false depending on if User was created or not.
      */
-    public int createUser(String _email, String _username, String _password) {
+    public static boolean createUser(String _email, String _username, String _password) {
         // Create HashMap and populate it with the information passed in from GUI and the string indicating what it is.
         HashMap<String, String> map = new HashMap();
         map.put("email", _email);
         map.put("username", _username);
         map.put("password", _password);
+        map.put("active", "true");
         // Int that equals the result of the DataBaseTranslator method call.
-        int checkDb = this.translate.createObject(map, "users");
-        // If checkDb = 1, a new User object has been created in the database so create a new User object and route to homescreen panel.
-        if (checkDb == 1) {
+        boolean checkDb = translate.createObject(map, "users");
+        // If checkDb = true, a new User object has been created in the database so create a new User object and retrurn true.
+        if (checkDb == true) {
             User user = new User(_email, _username, _password);
-            return AppMessage.HOME_SCREEN_PANEL;
-            // Else a user already existed with this information so return the int of the registration panel.
+            return true;
+            // Else a user already existed with this information so return false..
         } else {
-            return AppMessage.REGISTRATION_PANEL;
+            return false;
         }
 
     }
@@ -96,22 +94,21 @@ public class User {
      *
      * @param _attribute - The attribute the User wants changed.
      * @param _value - The value of the new attribute
-     * @return - Int indicating whether or not the information was changed or
-     * not.
+     * @return - True of object was updated and false if not.
      */
-    public int updateInfo(String _attribute, String _value) {
+    public boolean updateInfo(String _attribute, String _value) {
         // Create HashMap object with the attribute as the key and the value as the value.
         HashMap<String, String> map = new HashMap();
         map.put(_attribute, _value);
         // Boolean indicating true if change was made or false if not.
-        boolean checkDb = this.translate.updateObject(map, this.getUsername(), "users");
+        boolean checkDb = translate.updateObject(map, this.getUsername(), "users");
         // If statement for handling true or false from the database.
         if (checkDb == true) {
             // How will i know what part of the User object here?
-            return AppMessage.HOME_SCREEN_PANEL;
+            return true;
             // The information already exists so the update can not be performed.
         } else {
-            return 0;
+            return false;
         }
     }
 
@@ -119,21 +116,21 @@ public class User {
      * Method that deletes an account based on the username of the current
      * account.
      *
-     * @return - AppMessage int based on outcome of the DataBaseTranslator call.
+     * @return - True if account was deleted, false if not.
      */
-    public int deleteAccount() {
+    public boolean deleteAccount() {
         // Create a boolean variable that makes a call to the DataBaseTranslator class to delete the current account.
-        boolean deleteUser = this.translate.deleteObject(this.getUsername(), "users");
+        boolean deleteUser = translate.deleteObject(this.getUsername(), "users");
         // If object was deleted, set everything equal to null and return to the Login screen.
         if (deleteUser == true) {
             this.setEmail(null);
             this.setUsername(null);
             this.setPassword(null);
             // Object is deleted by garbage collector so return to Log in panel.
-            return AppMessage.LOG_IN_PANEL;
+            return true;
             // Object was not deleted so return to the Home screen panel with the account information.
         } else {
-            return AppMessage.HOME_SCREEN_PANEL;
+            return false;
         }
     }
 
