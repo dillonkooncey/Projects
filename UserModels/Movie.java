71c93d58dbf6ab35c86e9c5d3d0136a07679ab1e
dillonkooncey <46939222+dillonkooncey@users.noreply.github.com>
2019-11-25 16:35:47 -0500

    Update Movie.java

--- UserModels/Movie.java
+++ UserModels/Movie.java
@@ -2,7 +2,6 @@ package UserModels;
 
 import Api.ApiTranslator;
 import Events.AppBase;
-import Events.AppMessage;
 
 /**
  * Movie class that stores information about a searched movie's title, overview,
@@ -16,7 +15,7 @@ public class Movie extends AppBase {
     private double rating;
     private String name;
     // Give access to the Apitranslator class for API calls.
-    private ApiTranslator translate = new ApiTranslator();
+    private static ApiTranslator translate = new ApiTranslator();
 
     // Constructor for Movie object.
     public Movie(double _rating, String _name) {
@@ -31,13 +30,13 @@ public class Movie extends AppBase {
      * @param _movieName - Name of the movie searched by the User.
      * @return - int of the Movie panel
      */
-    public int findMovieRating(String _movieName) {
+    public static double findMovieRating(String _movieName) {
         // Get the rating of the movie searched by the User and store it as a double.
-        double movieRating = this.translate.findMovieRating(_movieName);
+        double movieRating = ApiTranslator.findMovieRating(_movieName);
         // Create a new Movie object with the double value of the searched movie.
         Movie movie = new Movie(movieRating, _movieName);
         // Return back to the movie rating panel.
-        return AppMessage.MOVIE_RATING_PANEL;
+        return movieRating;
     }
 
     // ========== SETTERS ==========//
