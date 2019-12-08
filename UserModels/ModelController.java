7c539adeb0ae1e74317cd4b1a986f23517221a07
dillonkooncey <46939222+dillonkooncey@users.noreply.github.com>
2019-12-08 15:13:06 -0500

    Update ModelController.java

--- UserModels/ModelController.java
+++ UserModels/ModelController.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
 public class ModelController {
 
     // Give access to models to fetch information.
-    private User user = new User();
+    private User user;
     private Actor actor = new Actor();
     private Movie movie = new Movie();
 
@@ -31,11 +31,12 @@ public class ModelController {
      * @param _password - password passed in from user in view.
      * @return - The integer of the panel to be routed to.
      */
-    public static int verifyUser(String _email, String _username, String _password) {
+    public int verifyUser(String _email, String _username, String _password) {
         // Boolean sending information to User class to see if account exists.
         boolean fetchVerification = User.validateUser(_email, _username, _password);
         // If account was verified go to home screen.
         if (fetchVerification == true) {
+            this.user = new User(_email, _username, _password);
             return AppMessage.HOME_SCREEN_PANEL;
             // Else go back to Log in screen to re-enter information.
         } else {
@@ -52,11 +53,12 @@ public class ModelController {
      * @param _password - Username passed in from User.
      * @return - The integer of the panel routed to.
      */
-    public static int createUser(String _email, String _username, String _password) {
+    public int createUser(String _email, String _username, String _password) {
         // Boolean sending information to User class to see if account can be created.
         boolean checkAccount = User.createUser(_email, _username, _password);
         // If account was created go to home screen. Else go back to registration screen.
         if (checkAccount == true) {
+            this.user = new User(_email, _username, _password);
             return AppMessage.HOME_SCREEN_PANEL;
         } else {
             return AppMessage.REGISTRATION_PANEL;
@@ -77,6 +79,7 @@ public class ModelController {
         boolean checkUpdate = this.user.updateInfo(_email, _username, _password);
         // If update was made route to home screen if not go back to options panel to re-enter information.
         if (checkUpdate == true) {
+            this.user = new User(_email, _username, _password);
             return AppMessage.HOME_SCREEN_PANEL;
         } else {
             return AppMessage.OPTIONS_PANEL;
@@ -94,6 +97,7 @@ public class ModelController {
         boolean deleteAccount = this.user.deleteAccount();
         // If account was deleted route to log in screen and home screen if not.
         if (deleteAccount == true) {
+            this.user = new User();
             return AppMessage.LOG_IN_PANEL;
         } else {
             return AppMessage.HOME_SCREEN_PANEL;
@@ -114,6 +118,7 @@ public class ModelController {
         boolean checkDeactivated = this.user.reactivateAccount(_email, _username, _password);
         // If account was reactivated go to home screen, if not go back to account recovery.
         if (checkDeactivated == true) {
+            this.user = new User(_email, _username, _password);
             return AppMessage.HOME_SCREEN_PANEL;
         } else {
             return AppMessage.ACCOUNT_RECOVERY;
@@ -135,7 +140,7 @@ public class ModelController {
      * @return - Username of User.
      */
     public String getUserUsername() {
-        return user.getUsername();
+        return this.user.getUsername();
     }
 
     /**
@@ -144,7 +149,7 @@ public class ModelController {
      * @return - Password of User.
      */
     public String getUserPassword() {
-        return user.getPassword();
+        return this.user.getPassword();
     }
 
     // ======= Movie class fetches ====== //
@@ -155,8 +160,10 @@ public class ModelController {
      * @param _movieName - Name of the movie searched.
      * @return - The integer of the movie rating panel.
      */
-    public static int findMovieRating(String _movieName) {
-        double findRating = Movie.findMovieRating(_movieName);
+    public int findMovieRating(String _movieName) {
+        this.movie = new Movie();
+        double findRating = this.movie.findMovieRating(_movieName);
+        this.movie = new Movie(findRating, _movieName);
         return AppMessage.MOVIE_RATING_PANEL;
     }
 
@@ -187,8 +194,10 @@ public class ModelController {
      * @param _actorName - Name of the actor searched by the User.
      * @return - The integer of the actors panel.
      */
-    public static int findMovieList(String _actorName) {
-        boolean findList = Actor.findMovieList(_actorName);
+    public int findMovieList(String _actorName) {
+        this.actor = new Actor();
+        ArrayList<String> findList = this.actor.findMovieList(_actorName);
+        this.actor = new Actor(findList, _actorName);
         return AppMessage.ACTORS_PANEL;
     }
 
@@ -218,7 +227,8 @@ public class ModelController {
      * @return - The list of movies in ascending order.
      */
     public ArrayList<String> sortAscendingOrder() {
-        return this.actor.sortAscendingOrder();
+        this.actor.sortAscendingOrder();
+        return this.actor.getMovies();
     }
 
     /**
@@ -228,6 +238,7 @@ public class ModelController {
      * @return - The list of movies in descending order.
      */
     public ArrayList<String> sortDescendingOrder() {
-        return this.actor.sortDescendingOrder();
+        this.actor.sortDescendingOrder();
+        return this.actor.getMovies();
     }
 }
