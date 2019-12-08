a7ec5f4b2f9f5aadec083d4be7b6f3bad72f2dea
dillonkooncey <46939222+dillonkooncey@users.noreply.github.com>
2019-12-07 19:55:37 -0500

    Update Tmdb.java

--- Api/Tmdb.java
+++ Api/Tmdb.java
@@ -6,7 +6,6 @@ import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -21,6 +20,7 @@ public class Tmdb implements ApiInterface {
     // Data fields for Tmdb class.
     private static final String apiKey = "68c9635b6b0bc9ccad7ea0b91402025b";
     private static final String baseUrl = "https://api.themoviedb.org";
+    private ArrayList<String> movieList = new ArrayList();
 
     /**
      * Method that takes in a movie String query and returns the rating of the
@@ -55,14 +55,17 @@ public class Tmdb implements ApiInterface {
             con.disconnect();
             // Extract the JSON object.
             JSONObject obj = new JSONObject(content.toString());
-            // Get the double value of the vote average for the movie entered.
-            double movieRating = obj.getDouble("vote_average");
+            // Create a JSON array of the results from the API call.
+            JSONArray array = obj.getJSONArray("results");
+            JSONObject ob = array.getJSONObject(0);
+            // Get the vote average from the JSONArray.
+            double movieRating = ob.getDouble("vote_average");
             // Return movie rating double value.
             return movieRating;
         } catch (Exception e) {
-            System.out.println("there was an error: " + e.getMessage());
+            System.out.println("Movie was not found.");
         }
-        return -1;
+        return 0;
     }
 
     /**
@@ -79,9 +82,13 @@ public class Tmdb implements ApiInterface {
             String newString = this.queryBuilder(_actor);
             // Build the search string to be used by the API.
             String searchString = "/3/search/person?api_key=" + Tmdb.apiKey + "&language=en-US&query=" + newString + "&page=1&include_adult=false";
+            // Create a new URL object with the baseUrl and created search string.
             URL url = new URL(Tmdb.baseUrl + searchString);
+            // Establish a HttpURLConnection with the created URL object.
             HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            // Set the request method.
             con.setRequestMethod("GET");
+            // Create a BufferedReader to read the character stream from the HttpURLConnection.
             BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
             String inputLine;
             // Build the content from the buffered input.
@@ -94,12 +101,25 @@ public class Tmdb implements ApiInterface {
             con.disconnect();
             // Extract the JSON object.
             JSONObject obj = new JSONObject(content.toString());
-            JSONArray array = obj.getJSONArray("original_title");
-            return null;
+            // Create a new JSONArray with the "results" array from JSONObject.
+            JSONArray array = obj.getJSONArray("results");
+            // Get the first element of the "results" array.
+            JSONObject first = array.getJSONObject(0);
+            // Create JSONArray with the "known_for" array from the "results" array.
+            JSONArray array2 = first.getJSONArray("known_for");
+            // Loop through the results array to find each movie the Actor has acted in.
+            for (int i = 0; i < array2.length(); i++) {
+                JSONObject test1 = array2.getJSONObject(i);
+                String test2 = test1.getString("title");
+                // Add the movie title to the ArrayList to then be returned.
+                this.movieList.add(test2);
+            }
+            return this.movieList;
         } catch (Exception e) {
-            System.out.println("There was an error: " + e.getMessage());
+
         }
-        return null;
+        this.movieList.add("Actor not found");
+        return this.movieList;
     }
 
     /**
@@ -110,8 +130,7 @@ public class Tmdb implements ApiInterface {
      */
     private String queryBuilder(String _string) {
         try {
-            // Return the String modified by the URLEncoder class.
-            return URLEncoder.encode(_string, StandardCharsets.UTF_8.toString());
+            return URLEncoder.encode(_string, "UTF-8").replace("+", "%20");
         } catch (UnsupportedEncodingException ex) {
             throw new RuntimeException(ex.getCause());
         }
