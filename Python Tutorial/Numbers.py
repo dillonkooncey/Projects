d4c0f7582e52b79653ea146b4585838a64b9fa86
dillonkooncey <46939222+dillonkooncey@users.noreply.github.com>
2021-01-31 00:57:53 -0500

    Create Numbers.py

---
+++ Python Tutorial/Numbers.py
@@ -0,0 +1,35 @@
+#Print out a number. Can be neagtive and have decimals.
+print(2)
+
+#Math functions (PEMDAS).
+print(10 % 3)
+
+#Store numbers in variables
+my_num = 5
+print(my_num)
+
+#Convert number to string. Have to convert a number to string if you want to print next to a string.
+print(str(my_num))
+
+#Absolute value of a number
+my_num = -5
+print(abs(my_num))
+
+#Power to...
+print(pow(5, 2))
+
+#Find max and min of two numbers
+print(max(4, 6))
+print(min(4, 6))
+
+#Round numbers
+print(round(3.7))
+
+# Import libraries
+from math import *
+#Find floor
+print(floor(3.7))
+
+#Find square root
+print(sqrt(36))
+
