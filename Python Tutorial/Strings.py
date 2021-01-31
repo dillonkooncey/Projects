d1d8668cf1ddc9b7c9219131a74daa1437bb9c3f
dillonkooncey <46939222+dillonkooncey@users.noreply.github.com>
2021-01-31 00:58:19 -0500

    Create Strings.py

---
+++ Python Tutorial/Strings.py
@@ -0,0 +1,28 @@
+# Simple print statement with new line "\n"
+print("My name is Dillon")
+
+# String variable
+phrase = "Howdy"
+print(phrase)
+
+#Concatenation
+hey = "Hey"
+print(phrase + " " + hey)
+
+# convert to lowercase
+hey.lower()
+# convert to upper case
+hey.upper()
+# check if entirely uppercase
+hey.isupper()
+# Finding number of characters in a string
+print(len(phrase))
+
+#Get a character in a string
+print(phrase[0])
+
+# Tell us where a specific character is located in a string
+print(phrase.index("H"))
+
+#Replace a character in a strig with another character
+print(phrase.replace("H", "A"))
