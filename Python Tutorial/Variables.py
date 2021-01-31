00ce540cf6264738cd47c1d2db02e7af4dfe2332
dillonkooncey <46939222+dillonkooncey@users.noreply.github.com>
2021-01-31 00:59:04 -0500

    Create Variables.py

---
+++ Python Tutorial/Variables.py
@@ -0,0 +1,12 @@
+character_name = "john"
+character_age = "35"
+
+print("There was once a man named " + character_name + ", ")
+print("He was " + character_age + " years old.")
+print("He really liked the name " + character_name + " , ")
+print("But he didn't like being " + character_age + ".")
+
+# Can use same name for variable. Will go line by line so changes can be made without creating a new variable.
+character_name = "Dillon"
+print(character_name)
+# Prints Dillon now because we have "updated" character_name's value.
