2ae25e7941b61c1fd9b9ae30ec75f768ce1c91ad
dillonkooncey <46939222+dillonkooncey@users.noreply.github.com>
2021-02-03 00:07:10 -0500

    Create Return Statement.py

---
+++ Python Tutorial/Return Statement.py
@@ -0,0 +1,9 @@
+#return information from a function
+def cube(number):
+    #Without return nothing will be displayed in the pritn statement below.
+    return number * number * number
+
+print(cube(3))
+#Can also be used in this way.
+result = cube(4)
+print(result)
